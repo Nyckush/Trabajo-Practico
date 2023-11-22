ce6375f989dd331f96c4e1d43f3309634a2b6d72
nicolas bacalini <nicolasbacalini2@gmail.com>
2023-11-22 16:41:33 -0300

    a

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { About } from './componentes/About/About.jsx'
+import { Contact } from './componentes/Contact/Contact.jsx'
+import { Footer } from './componentes/Footer/Footer.jsx'
+import { Header } from './componentes/Header/Header.jsx'
+import { Proyect } from './componentes/Proyect/Proyect.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Header/>
-    <Proyect/>
-    <Footer/>
-    <About/>
-  </React.StrictMode>,
+    <About />
+    <Contact />
+    <Footer />
+    <Header />
+    <Proyect />
+  </React.StrictMode>
 )

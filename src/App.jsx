0844ac5a74136edf2c71c0d8fc00d904593a951e
Pablo-Fornerod <126510924+Pablo-Fornerod@users.noreply.github.com>
2023-11-23 17:10:19 -0300

    a
    :sheep:

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,62 @@ import './App.css'
 import { TaskList, TaskForm, TaskItem } from './componentes/Index'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formVisible, setFormVisible] = useState(false)
+  const visible = () => {
+    setFormVisible(!formVisible)
+  }
+
+  const [tasks, setTasks] = useState([]);
+
+  function addTask(task, tag) {
+    setTasks([
+      ...tasks, 
+      {
+        id: crypto.randomUUID(),
+        task: task,
+        tag: tag,
+        isCompleted: false
+      }
+    ]);
+  }
+
+  function toggleTaskCompletedById(taskId) {
+    const newTasks = tasks.map(task => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted }
+      }
+      return task;
+    })
+    setTasks(newTasks);
+  }
 
   return (
-    <>
-      <TaskList />
-      <TaskForm />
-      <TaskItem />
-    
-      <h1>hola</h1>
-    </>
+    <div className='contenedor'>
+      <section>
+            <header>
+                <div>
+                    <p>Superlist</p>
+                </div>
+                <div>
+                    <p>List</p>
+                </div>
+                <div>
+                    <p>Tags</p>
+                </div>
+            </header>
+
+        </section>
+      <TaskList 
+        tasks={tasks}
+        onComplete={toggleTaskCompletedById} 
+        />
+        {
+            formVisible && (
+              <TaskForm onAddTask={addTask} />
+            )
+          }
+          <button onClick={visible}>Add</button>
+    </div>
   )
 }
 
--- src/componentes/TaskForm/TaskForm.jsx
+++ src/componentes/TaskForm/TaskForm.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
 
-const TaskForm = () => {
+const TaskForm = ({ onAddTask }) => {
+
+    const [task, setTask] = React.useState("");
+    function handleSubmit(event) {
+        event.preventDefault();
+        onAddTask(task, tag);
+
+        setTask("");
+        setTag("");
+    }
+
+    function onChangeTask(event) {
+        setTask(event.target.value);
+    }
+    const [tag, setTag] = React.useState("");
+
+    function onChangeTag(event) {
+        setTag(event.target.value);
+    }
+
     return(
-        <div>TaskForm</div>
+        <form onSubmit={handleSubmit}>
+            <input placeholder='Name' type="text" value={task} onChange={onChangeTask} />
+
+            <input placeholder='New tag' type="text" value={tag} onChange={onChangeTag} />
+            <button>+</button>
+
+            <button>Save</button>
+        </form>
     )
 }
 
